91c29f36a7b7e5299d8c41da19a80da0d484fdc5
akif-bin-sayeed <akif.bin.sayeed01@gmail.com>
2026-06-11 06:03:01 +0600

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,5 +0,0 @@
-window.PORTFOLIO_CONFIG = {
-  supabaseUrl: 'https://xlytrnbstwlcqzvuivwa.supabase.co',
-  supabaseAnonKey: 'sb_publishable_AbxXmCI1iQqFelBFPXlQxw_4WbtFHpZ',
-  adminEmail: 'akif.sayeed01@gmail.com'
-};
